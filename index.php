38631dbd60aa9ed0140182c60d5e16b7711bd9e8
CasualJon <jon@joncyrus.com>
2019-02-09 14:44:22 -0600

    Testing continue button offset

--- index.php
+++ index.php
@@ -82,8 +82,8 @@
       </div> <!-- /row -->
 
       <div class="row">
-        <div class="col-md-4 col-md-offset-4">
-          <button class="btn btn-lg btn-outline-danger">CONTINUE</button>
+        <div class="col-md-4 offset-me-4">
+          <button class="btn btn-lg btn-outline-danger"><h2>CONTINUE</h2></button>
         </div> <!-- /column -->
       </div> <!-- /row -->
 
