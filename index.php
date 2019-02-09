528e80d4c097816303cd196761de8aa88609dac7
CasualJon <jon@joncyrus.com>
2019-02-09 14:48:19 -0600

    Tweaks to continue button

--- index.php
+++ index.php
@@ -83,7 +83,7 @@
 
       <div class="row">
         <div class="col-md-4 offset-md-4">
-          <button class="btn btn-lg btn-outline-danger"><b style="font-size: 22px;">CONTINUE<b></button>
+          <button class="btn btn-lg btn-outline-danger"><b style="font-size: 44px; width: 100%">CONTINUE<b></button>
         </div> <!-- /column -->
       </div> <!-- /row -->
 
