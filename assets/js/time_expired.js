f641a22b5c7a75a96ac96cbd497d08f6e3592296
CasualJon <jon@joncyrus.com>
2019-02-10 12:28:15 -0600

    typo

--- assets/js/time_expired.js
+++ assets/js/time_expired.js
@@ -2,5 +2,5 @@
 //If the user sits idle on a page greater than the time allowed for that page's
 //content, then this function is called by the JS of that page to call it quits
 function elapseSurvey() {
-  alert("Time's up").
+  alert("Time's up");
 } //END elapseSurvey()
