1499c57c2e7ea598c8cdacbf29beb6eb3dc291ad
CasualJon <jon@joncyrus.com>
2019-02-10 11:56:18 -0600

    test

--- php_includes/db.php
+++ php_includes/db.php
@@ -1,11 +1,11 @@
 <?php
   //Control vars and database connection settings
-  require '/php_includes/control_variables.php';
-  $host = "localhost";
+  require './control_variables.php';
+  $db_host = "localhost";
 
   //Check to see if $mysql already exists, instantiate if not
   if (!isset($mysqli)) {
-    $mysqli = new mysqli($host, $db_user, $db_pass, $db_name);
+    $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
     if (!$mysqli->set_charset("utf8mb4")) {
       $db_issue = "Database error loading UTF8 character set.<br /> We're working on it, but please feel free to reach out and let us know about Error Code #".$mysqli->errno;
     }
@@ -19,7 +19,7 @@
     if (!empty($_SESSION['message'])) $_SESSION['message'] += "<br />".$db_issue;
     else $_SESSION['message'] = $db_issue;
 
-    header("location: /error.php");
+    header("location: ../error.php");
     exit;
   }
 ?>
