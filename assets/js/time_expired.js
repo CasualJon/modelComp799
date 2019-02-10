626b503b775a2a554f1aaf89e611963699e6cfde
CasualJon <jon@joncyrus.com>
2019-02-10 12:13:18 -0600

    Add time_expired.js

---
+++ assets/js/time_expired.js
@@ -0,0 +1,6 @@
+//elapseSurvey()
+//If the user sits idle on a page greater than the time allowed for that page's
+//content, then this function is called by the JS of that page to call it quits
+function elapseSurvey() {
+  alert("Time's up").
+} //END elapseSurvey()
