51674c98380ec238b843d555976e6b0316e8d72c
CasualJon <jon@joncyrus.com>
2019-02-10 11:32:47 -0600

    Remove debug comments

--- assets/js/informed_consent.js
+++ assets/js/informed_consent.js
@@ -3,13 +3,10 @@ document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('consent_agree').onchange = updateContinueButton;
 },false);
 
-console.log("IN JS FILE");
-
 //updateContinueButton()
 //Called by event listener on Informed Consent Agreement checkbox - toggles
 //disabled status of button when consent agreement is given
 function updateContinueButton() {
-  console.log("checked = " + document.getElementById('consent_agree').checked);
   var continueButton = document.getElementById('continue_button');
   if (document.getElementById('consent_agree').checked) {
     continueButton.disabled = false;
