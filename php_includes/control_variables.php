658ed267a5b54b45cc210379668ad9536babf8d4
CasualJon <jon@joncyrus.com>
2019-02-11 10:11:33 -0600

    Updates to control vars to include survey data info

--- php_includes/control_variables.php
+++ php_includes/control_variables.php
@@ -1,7 +1,15 @@
 <?php
+  //The domain this experiement is hosted on
   $localhost_domain = "quickfeedback.io";
+  //The domain this experiment will allow referral access from
   $allowed_ext_refer = "mturk.com";
+
+  //Database informaiton for mysqli access
   $db_name = "model_understanding_spring19";
   $db_user = "phpmyadmin";
   $db_pass = "cs799";
+
+  //Directory information for the images the classifier/user will evaluate
+  $img_source = "./assets/img/lyrics/";
+  $num_questions = 20;
 ?>
