46c08f61594c52c21a2150f9e78861aebfb38233
CasualJon <jon@joncyrus.com>
2019-02-11 10:11:01 -0600

    update time_expired to include max idle var

--- assets/js/time_expired.js
+++ assets/js/time_expired.js
@@ -1,3 +1,7 @@
+//System-wide max allowable time (when evaluated) that a user is allowed to
+//remain unresponsive on a page.
+var maxMillis = 16000;
+
 //elapseSurvey()
 //If the user sits idle on a page greater than the time allowed for that page's
 //content, then this function is called by the JS of that page to call it quits
