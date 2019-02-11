8f69de9664da4b0e68b41964d67696337c417ab0
CasualJon <jon@joncyrus.com>
2019-02-11 10:38:11 -0600

    Testing array removal w/ array_splice

--- php_includes/survey_data.php
+++ php_includes/survey_data.php
@@ -7,14 +7,16 @@
   $dir_contents = scandir($img_source);
 
   //Remove the current and parent directories from the array...
-  if (($key = array_search(".", $dir_contents)) !== false) {
-    unset($dir_contents[$key]);
-  }
-  if (($key = array_search("..", $dir_contents)) !== false) {
-    unset($dir_contents[$key]);
-  }
-  unset($key);
-  
+  // if (($key = array_search(".", $dir_contents)) !== false) {
+  //   unset($dir_contents[$key]);
+  // }
+  // if (($key = array_search("..", $dir_contents)) !== false) {
+  //   unset($dir_contents[$key]);
+  // }
+  // unset($key);
+  //Default sorting of scandir() returns these as elements 0 & 1
+  $dir_contents = array_splice($dir_contents, 0, 2);
+
   $_SESSION['exp_data'] = $dir_contents;
 
 
